1201556c0560d667d71fa54ea896bec853adb3e4
Zerobase42 <malbolgeisfun@naver.com>
2025-10-19 16:24:02 +0900

    console setting + input,output faster

--- cube_game.cpp
+++ cube_game.cpp
@@ -19,7 +19,14 @@ int GetKeyDown(){
         return _getch();
     return 0;
 }
+void SetConsoleView(){
+    system("mode con:cols=50 lines=20");
+    system("title [CubeGame] by.0B42");
+}
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     return 0;
 }

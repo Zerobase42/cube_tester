c9a013373a6b04b59de680a605b6757aa1280ddf
Zerobase42 <malbolgeisfun@naver.com>
2025-10-21 10:43:04 +0900

    add color text SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);

--- cube_game.cpp
+++ cube_game.cpp
@@ -15,7 +15,27 @@ enum{
     DOWN_KEY=280
 };
 
-
+void textcolor(int colorNum) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
+}
+enum {
+    BLACK,
+    DARK_BLUE,
+    DARK_GREEN,
+    DARK_SKYBLUE,
+    DARK_RED,
+    DARK_VOILET,
+    DAKR_YELLOW,
+    GRAY,
+    DARK_GRAY,
+    BLUE,
+    GREEN,
+    SKYBLUE,
+    RED,
+    VIOLET,
+    YELLOW,
+    WHITE,
+};
 
 int selectLetteringScheme;
 int UP_COLOR=0,FRONT_COLOR=2;

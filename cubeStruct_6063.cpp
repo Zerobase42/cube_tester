3f843e2a5e4d2430ce621507ccf3b29f73d1e10b
Zerobase42 <malbolgeisfun@naver.com>
2026-05-26 20:35:36 +0900

    faster

--- cubeStruct_6063.cpp
+++ cubeStruct_6063.cpp
@@ -1,12 +1,14 @@
 #include <cstdio>
-#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
 using namespace std;
 class Cube{
+#define C(f,r,c) cube[f*9+r*3+c]
 public:
     char cube[6][3][3];
 private:
-    static const char* color="wyrogb";
-    static const int ord[4]={4,2,5,3};
+    const char* color="wyrogb";
+    static constexpr int ord[4]={4,2,5,3};
 public:
     void read_cube(){
         for(int i=0;i<3;i++)for(int j=0;j<3;j++)scanf(" %c",&cube[0][i][j]);
@@ -40,57 +42,65 @@ public:
     }
 private:
     void rotate_face(int f){
-        int C[3][3];
-        for(int i=0;i<3;i++)for(int j=0;j<3;j++)C[i][j]=cube[f][i][j];
-        for(int i=0;i<3;i++)for(int j=0;j<3;j++)cube[f][j][2-i]=C[i][j];
+        char t;
+        t=cube[f][0][0];
+        cube[f][0][0]=cube[f][2][0];
+        cube[f][2][0]=cube[f][2][2];
+        cube[f][2][2]=cube[f][0][2];
+        cube[f][0][2]=t;
+        t=cube[f][0][1];
+        cube[f][0][1]=cube[f][1][0];
+        cube[f][1][0]=cube[f][2][1];
+        cube[f][2][1]=cube[f][1][2];
+        cube[f][1][2]=t;
     }
 public:
     void turn(int s,int d){
-        if(d!=1){
-            turn(s,1);turn(s,1);
-            if(d==-1)turn(s,1);
-            return;
-        }
-        rotate_face((34644>>(s*3))&7);
-        if(s==4||s==5){
-            int a=(s-4)<<1;
-            const int ts[2][4]={{2,5,3,4},{2,4,3,5}};
-            const int *p=ts[s-4];
-            for(int i=0,t;i<3;i++){
-                t=cube[p[0]][a][i];
-                for(int j=0;j<3;j++)cube[p[j]][a][i]=cube[p[j+1]][a][i];
-                cube[p[3]][a][i]=t;
-            }
-        }
         static const int ri[2][3]={{0,1,2},{2,1,0}};
-        static constexpr int *p,*q;
-        if(s==1||s==3){
-            int C[3],t=s>>1;
-            static constexpr int t1[2]={0,2},t2[2]={4,5};
-            *p=ri[t],*q=ri[t^1];
-            for(int i=0;i<3;i++){
-                C[i]=cube[0][t1[t^1]][i];
-                cube[0][t1[t^1]][i]=cube[t2[t]][q[i]][2];
-            }
-            for(int i=0;i<3;i++){
-                cube[t2[t]][i][2]=cube[1][t1[t]][p[i]];
-                cube[1][t1[t]][p[i]]=cube[t2[t^1]][2-i][0];
-            }
-            for(int i=0;i<3;i++)
-                cube[t2[t^1]][p[i]][0]=C[i];
-        }
-        if(s==0||s==2){
-            int C[3],t=s>>1;
-            static constexpr int t3[]={2,3};
-            *p=ri[t],*q=ri[t^1];
-            for(int i=0;i<3;i++){
-                C[i]=cube[0][i][s];
-                cube[0][i][s]=cube[t3[t^1]][q[i]][2];
-            }
-            for(int i=0;i<3;i++)cube[t3[t^1]][i][2]=cube[1][q[i]][s];
-            for(int i=0;i<3;i++){
-                cube[1][i][s]=cube[t3[t]][p[i]][0];
-                cube[t3[t]][p[i]][0]=C[i];
+        const int *p,*q;
+        int cnt=(d==2)?2:(d==-1?3:1);
+        while(cnt--){
+            rotate_face((34644>>(s*3))&7);
+            switch(s){
+            case 4:case 5:{
+                int a=(s-4)<<1;
+                const int ts[2][4]={{2,5,3,4},{2,4,3,5}};
+                p=ts[s-4];
+                for(int i=0,t;i<3;i++){
+                    t=cube[p[0]][a][i];
+                    for(int j=0;j<3;j++)cube[p[j]][a][i]=cube[p[j+1]][a][i];
+                    cube[p[3]][a][i]=t;
+                }
+            }break;
+            case 1:case 3:{
+                int C[3],t=s>>1;
+                static constexpr int t1[2]={0,2},t2[2]={4,5};
+                p=ri[t];q=ri[t^1];
+                for(int i=0;i<3;i++){
+                    C[i]=cube[0][t1[t^1]][i];
+                    cube[0][t1[t^1]][i]=cube[t2[t]][q[i]][2];
+                }
+                for(int i=0;i<3;i++){
+                    cube[t2[t]][i][2]=cube[1][t1[t]][p[i]];
+                    cube[1][t1[t]][p[i]]=cube[t2[t^1]][2-i][0];
+                }
+                for(int i=0;i<3;i++)
+                    cube[t2[t^1]][p[i]][0]=C[i];
+            }break;
+            case 0:case 2:{
+                int C[3],t=s>>1;
+                static constexpr int t3[]={2,3};
+                p=ri[t];q=ri[t^1];
+                for(int i=0;i<3;i++){
+                    C[i]=cube[0][i][s];
+                    cube[0][i][s]=cube[t3[t^1]][q[i]][2];
+                }
+                for(int i=0;i<3;i++)cube[t3[t^1]][i][2]=cube[1][q[i]][s];
+                for(int i=0;i<3;i++){
+                    cube[1][i][s]=cube[t3[t]][p[i]][0];
+                    cube[t3[t]][p[i]][0]=C[i];
+                }
+            }break;
             }
         }
     }
